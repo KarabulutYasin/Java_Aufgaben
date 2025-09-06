5edf8059c8eb67b4b7a000c55564b0ba4f1ad4e0
Yasin Karabulut <yasin.karabulut@alumni.fh-aachen.de>
2025-09-06 10:40:27 +0200

    Array Aufgaben

--- Aufgaben/src/main/java/de/aufgabe9/AliasingDemo.java
+++ Aufgaben/src/main/java/de/aufgabe9/AliasingDemo.java
@@ -7,6 +7,7 @@ public class AliasingDemo {
     public int[] copyArray(int[] original) {
         // TODO Gib eine echte Kopie des Arrays zurück
         // Stichwort Aliasing
+        // Stichwort Referenz
 
         return null;
     }

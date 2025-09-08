cfeddf1ada5c49c9d07fbf0e7d71007aed526476
Yasin Karabulut <yasin.karabulut@alumni.fh-aachen.de>
2025-09-08 18:36:32 +0200

    Aufgaben Stellung angepasst in Main und Aufgaben2.java

--- loesungen/src/main/java/de/block1/loesungen2/Aufgaben2.java
+++ loesungen/src/main/java/de/block1/loesungen2/Aufgaben2.java
@@ -8,19 +8,28 @@ import java.util.stream.Collectors;
 
 public class Aufgaben2 {
     public String echoNextToken(Scanner sc) {
-        //TODO Schreibe eine Funktion die eine Zahl von der Konsole einliest (Scanner) und wieder ausgibt
+        //TODO
+        // Gegeben ist eine Instanz von Scanner
+        // Es soll der nächste Token (String) eingelesen und zurückgegeben werden
 
         return  sc.hasNext() ? sc.next() : "";
     }
 
     public int absInt(int x) {
+        // Tipp: Siehe dir die Math library von Java an
+        // https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+
+        //TODO
+        // Gebe denn Betrag von x an
         //TODO Implementiere eine Funktion die den Betrag einer Zahl berechnet (Math.abs)
 
         return Math.abs(x);
     }
 
     public List<Integer> numbersOneToTen() {
-        //TODO Schreibe eine Methode die alle Zahlen von 1 bis 10 in einer Liste zurückgibt
+        //TODO
+        // Schreibe eine Methode die alle Zahlen von 1 bis 10 in eine Liste schreibt
+        // und diese Liste zurückgibt
 
         List<Integer> list = new ArrayList<>();
         for (int i = 1; i <= 10; i++) list.add(i);
@@ -28,7 +37,9 @@ public class Aufgaben2 {
     }
 
     public List<String> readUntilStop(Scanner sc) {
-        //TODO Schreibe eine Methode die solange ein input in eine Liste schreibt bis der Benutzer "stop" eingibt und dann die Liste zurückgibt
+        //TODO
+        // Schreibe eine Methode die solange ein input in eine Liste schreibt bis der Benutzer "stop" eingibt
+        // und dann die Liste zurückgibt
 
         List<String> result = new ArrayList<>();
         while (sc.hasNextLine()) {
@@ -40,7 +51,14 @@ public class Aufgaben2 {
     }
 
     public String matrixToString(int[][] m) {
-        //TODO Schreibe eine Methode die eine 2D-Matrix (int[][]) ausgibt
+        //TODO
+        // Schreibe eine Methode die die Werte einer 2D-Matrix (int[][]) als String zurückgibt
+
+        //Bsp
+        // Pseudo Array {{1,2,3},{4,5,6}}
+        // =>
+        // 1 2 3
+        // 4 5 6
 
         if (m == null) return "";
 

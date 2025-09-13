8ecc9d0e4b6ff3179abc580d1d1f89dc557398fb
Yasin Karabulut <yasin.karabulut@alumni.fh-aachen.de>
2025-09-13 12:34:10 +0200

    typkonversion eingefügt

---
+++ Aufgaben/src/main/java/de/a/fundamentals/c/typkonversionen/Typkonversion.java
@@ -0,0 +1,53 @@
+package de.a.fundamentals.c.typkonversionen;
+
+public class Typkonversion {
+
+    public int convertIntToShort(short x){
+        //TODO
+        // Konvertiere short x zu int und gebe es zurück
+
+        return -1;
+    }
+
+    public int convertDoubleToInt(double x){
+        //TODO
+        // Konvertiere denn double x in ein int
+
+        return -1;
+    }
+
+    public long convertIntToLong(int x){
+        //TODO
+        // Konvertiere int x zu long und gebe es zurück
+
+        return -1;
+    }
+
+    public float convertIntToFloat(int x){
+        //TODO
+        // Konvertiere int x zu float und gebe es zurück
+
+        return -1;
+    }
+
+    public double convertFloatToDouble(float x){
+        //TODO
+        // Konvertiere float x zu double und gebe es zurück
+
+        return -1;
+    }
+
+    public byte convertIntToByte(int x){
+        //TODO
+        // Konvertiere int x zu byte und gebe es zurück
+
+        return -1;
+    }
+
+    public char convertIntToChar(int x){
+        //TODO
+        // Konvertiere int x zu char und gebe es zurück
+
+        return 0;
+    }
+}

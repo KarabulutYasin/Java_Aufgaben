d2750c86493d353c7b2ea1a4b970dea4029b03d2
Yasin Karabulut <yasin.karabulut@alumni.fh-aachen.de>
2025-10-01 20:51:47 +0200

    broken commit aufgaben day 10 übersetzten

--- Aufgaben/src/main/java/de/month_1/fundamentals/day_10/modulo/ModMath.java
+++ Aufgaben/src/main/java/de/month_1/fundamentals/day_10/modulo/ModMath.java
@@ -3,21 +3,20 @@ package de.month_1.fundamentals.day_10.modulo;
 public class ModMath {
 
     //TODO
-    // Finde heraus was Modulo rechnung ist
-    // Finde heraus welche modulo Art Java verwendet
-    // Die methode soll a mod m im positiven Bereich zurückgeben
-    // Falls m <= 0 soll eine IllegalArgumentException geworfen werden
+    // Find out what modulo calculation is
+    // Find out which type of modulo Java uses
+    // The method should return a mod m in the positive range
+    // If m <= 0 an IllegalArgumentException should be thrown
 
-
-    public int normalizeMod(int a, int m) {
+    public int normalizeModulo(int a, int m) {
 
         return 0;
     }
 
     //TODO
-    // Schreibe eine Methode die Stunden im 12h Format addiert
+    // Write a method that adds hours in 12h format
 
-    // Bsp:
+    // Example:
     // addHours12(12, 1) -> 1
     // addHours12(3, -4) -> 11
 
@@ -26,13 +25,13 @@ public class ModMath {
     }
 
     public static void main(String[] args) {
-        // Hier ist nichts zu bearbeiten
+        // Nothing to edit here
         ModMath modMath = new ModMath();
         String divider = "----------------------";
 
         System.out.println(divider);
-        System.out.println("normalizeMod with parameters 10,4:");
-        System.out.println(modMath.normalizeMod(10,4));
+        System.out.println("normalizeModulo with parameters 10,4:");
+        System.out.println(modMath.normalizeModulo(10,4));
 
         System.out.println(divider);
         System.out.println("addHours12 with parameters 11, 9");

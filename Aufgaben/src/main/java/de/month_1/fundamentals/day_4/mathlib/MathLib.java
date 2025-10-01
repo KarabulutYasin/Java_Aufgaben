70f9397f207676f2696461138f1afdd15f0fa7cc
Yasin Karabulut <yasin.karabulut@alumni.fh-aachen.de>
2025-10-01 19:46:28 +0200

    Aufgaben day 4 Übersetzt

--- Aufgaben/src/main/java/de/month_1/fundamentals/day_4/mathlib/MathLib.java
+++ Aufgaben/src/main/java/de/month_1/fundamentals/day_4/mathlib/MathLib.java
@@ -7,66 +7,53 @@ import java.lang.reflect.Method;
 public class MathLib {
 
     //TODO
-    // Mach dich mit der Math library von Java vertraut um die folgenden Aufgaben zu lösen
+    // Get familiar with Java's Math library to solve the following tasks
     // https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Math.html
 
     //TODO
-    // Berechne die Wurzel einer Zahl
-    public double sqrt(double x) {
-        return 0;
+    // Calculate the square root of a number
+    public double squareRoot(double value) {return 0;}
+
+    //TODO
+    // Calculate a to the power of b
+    public double power(double base, double exponent) {return 0;}
+
+    //TODO
+    // Round a decimal number to the nearest whole number
+    public long round(double value) {return 0;
     }
 
     //TODO
-    // Berechne a hoch b
-    public double pow(double a, double b) {
-        return 0;
-    }
+    // Calculate the absolute value of a number
+    public int absoluteInt(int value) {return 0;}
 
     //TODO
-    // Runde eine Kommazahl auf die nächste ganze Zahl
-    public long round(double x) {
-        return 0;
-    }
+    // Calculate the sine of a number
+    public double sine(double radians) {return 0;}
 
     //TODO
-    // Berechne den Betrag einer Zahl
-    public int absInt(int x) {
-        return 0;
-    }
+    // Generate a random number between 0 (inclusive) and 1 (exclusive)
+    public double randomZeroToOne(){return -1;}
 
     //TODO
-    // Berechne den Sinus einer Zahl (in Radiant)
-    public double sin(double rad) {
-        return 0;
-    }
-
-    //TODO
-    // Erzeuge eine Zufallszahl zwischen 0 (inkl.) und 1 (exkl.)
-    public double random01() {
-        return -1;
-    }
-
-    //TODO
-    // Erzeuge eine ganze Zufallszahl im Bereich [min, max]
-    public int randomInt(int min, int max) {
-        return 0;
-    }
+    // Generate a random integer in the range [min, max]
+    public int randomInt(int min, int max) {return 0;}
 
     public static void main(String[] args) {
-        //Muss nicht bearbeitet werden
+        //No need to edit
         Method[] methods = MathLib.class.getDeclaredMethods();
-        Class<?> mathLibClass = MathLib.class;
+        Class<?> mathLibraryClass = MathLib.class;
 
         MainProvider.printAscii();
         for(Method method : methods) {
             if("main".equals(method.getName())) continue;
-            if("sqrt".equals(method.getName())) MainProvider.exec(method, mathLibClass, 16);
-            if("pow".equals(method.getName())) MainProvider.exec(method, mathLibClass, 2, 3);
-            if("round".equals(method.getName())) MainProvider.exec(method, mathLibClass, 3.6);
-            if("absInt".equals(method.getName())) MainProvider.exec(method, mathLibClass, -5);
-            if("sin".equals(method.getName())) MainProvider.exec(method, mathLibClass, Math.PI / 2);
-            if("random01".equals(method.getName())) MainProvider.exec(method, mathLibClass);
-            if("randomInt".equals(method.getName())) MainProvider.exec(method, mathLibClass, 5, 15);
+            if("squareRoot".equals(method.getName())) MainProvider.exec(method, mathLibraryClass, 16);
+            if("power".equals(method.getName())) MainProvider.exec(method, mathLibraryClass, 2, 3);
+            if("round".equals(method.getName())) MainProvider.exec(method, mathLibraryClass, 3.6);
+            if("absoluteInt".equals(method.getName())) MainProvider.exec(method, mathLibraryClass, -5);
+            if("sine".equals(method.getName())) MainProvider.exec(method, mathLibraryClass, Math.PI / 2);
+            if("randomZeroToOne".equals(method.getName())) MainProvider.exec(method, mathLibraryClass);
+            if("randomInt".equals(method.getName())) MainProvider.exec(method, mathLibraryClass, 5, 15);
         }
     }
 }
